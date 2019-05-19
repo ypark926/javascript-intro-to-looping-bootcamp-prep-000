590062e9821b0ca7a1bc936e22292c824b910a65
Yooni Park <yoonikj@gmail.com>
2019-05-19 20:15:20 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -23,7 +23,7 @@ function whileLoop(num) {
 function doWhileLoop(num) {
   do {
     console.log("I run once regardless.");
-  } while (incrementVariable() < num);
+  } while (incrementVariable() <= num);
 }
 
 var doint = 0;
